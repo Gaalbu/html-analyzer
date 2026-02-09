825951e96771c7887669392bafd99d519d497437
Gaalbu <gabrielalbuquerquealencar@gmail.com>
2026-02-09 15:24:35 -0300

    fix: corrigindo bug de leitura de atributos

--- HtmlAnalyzer.java
+++ HtmlAnalyzer.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ConnectException;
 import java.net.URI;
-import java.net.URL;
 import java.util.Stack;
 
 public class HtmlAnalyzer {
@@ -12,12 +11,11 @@ public class HtmlAnalyzer {
         if (args.length == 0) return; //url inválida
         String urlString = args[0];
         Stack<String> pilhaTags = new Stack<>();
-        int profundidadeMaxima = -1;
-        int profundidadeAtual = -1;
+        int profundidadeMaxima = 0;
+        int profundidadeAtual = 0;
         String textoMaisProfundo = "";
 
-        try {
-            BufferedReader conteudoUrl = new BufferedReader(new InputStreamReader(new URI(urlString).toURL().openStream()));
+        try (BufferedReader conteudoUrl = new BufferedReader(new InputStreamReader(new URI(urlString).toURL().openStream()))){
             String linhaAtual;
             while ((linhaAtual = conteudoUrl.readLine()) != null) {
                 linhaAtual = linhaAtual.trim(); // s/indentação
@@ -25,42 +23,46 @@ public class HtmlAnalyzer {
                 
                 if(linhaAtual.startsWith("<") ){ 
                     if(linhaAtual.startsWith("</")){ 
-                        //fechamento
+                        //compara c uma tag de abertura anterior
                         if (pilhaTags.isEmpty()){
                             System.out.println("malformed HTML");
                             return;
                         }
-
                     
                     String tagAberta = pilhaTags.pop();
-                    if(!tagAberta.replace("<","").replace(">", "")
-                            .equals(linhaAtual.replace("/", "").replace(">",""))){
-                        System.out.println("malformedHTML");
+                    String nomeTagFechamento = linhaAtual.substring(2, linhaAtual.length() - 1).trim();
+
+                    if(!tagAberta.equals(nomeTagFechamento)){
+                        System.out.println("malformed HTML");
                         return;
                     }
+
                     profundidadeAtual--;
                     }else{  
-                        //tag abertura
-                        pilhaTags.push(linhaAtual);
+                        //adiciona tag
+                        String conteudoTag = linhaAtual.substring(1, linhaAtual.length()-1);
+                        String nomePuro = conteudoTag.split(" ")[0]; // to tratando aqui para não pegar atributos de tag, ex: style type
+                        pilhaTags.push(nomePuro);
                         profundidadeAtual++;
-                        profundidadeMaxima = profundidadeMaxima < profundidadeAtual ? profundidadeAtual : profundidadeMaxima;
                     }
                 }else{
                     //aqui é o texto de fato
                     if (profundidadeAtual > profundidadeMaxima){
                         profundidadeMaxima = profundidadeAtual;
                         textoMaisProfundo = linhaAtual;
-                    }else if (profundidadeAtual == profundidadeMaxima && textoMaisProfundo.isEmpty()){
-                        textoMaisProfundo = linhaAtual;
                     }
                 }
             }
             
-            System.out.println(!pilhaTags.isEmpty() ? "malformedHTML" : textoMaisProfundo);
+            System.out.println(!(pilhaTags.isEmpty()) ? "malformed HTML" : textoMaisProfundo);
 
         } catch (ConnectException connectionError){
             System.err.println("URL connection error");
 
+        } catch (IOException ioExceptionError){ //Erro mais abrangente de leitura
+            System.err.println("URL connection error");
+        } catch (Exception e){ //Erro de parsing some aqui, virando malformed
+            System.out.println("malformed HTML");
         }
 
     }
